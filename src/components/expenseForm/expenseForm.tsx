50c70dd5928edfebd13f82fc5338ac7f62233c6c
Mohammed Askar <mdaskar40@gmail.com>
2023-10-25 17:17:57 +0530

    EXPEnse form updated

--- src/components/expenseForm/expenseForm.tsx
+++ src/components/expenseForm/expenseForm.tsx
@@ -10,16 +10,16 @@ const expenseForm = () => {
       <Container className="mt-5">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
+            <Form.Label>Description</Form.Label>
+            <Form.Control type="text" placeholder="" />
+            {/* <Form.Text className="text-muted">
               We'll never share your email with anyone else.
-            </Form.Text>
+            </Form.Text> */}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>Amount</Form.Label>
+            <Form.Control type="number" placeholder="$" />
           </Form.Group>
           <Form.Group className="mb-3">
             <Form.Label>Category</Form.Label>

ef03feebd771273916768244e495709640d8a6fc
Mohammed Askar <mdaskar40@gmail.com>
2023-10-27 16:23:11 +0530

    add filter table data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import "./App.css";
 import ExpenseForm from "./components/expenseForm";
+import TableFilter from './components/tableFilter';
 
 interface datas {
   description: string;
@@ -9,12 +10,23 @@ interface datas {
 }
 
 function App() {
-  // const [data,setData]=useState();
   const [data, setFormSubmissions] = useState<datas[]>([]);
 
   const dataHandler = (data: datas) => {
     setFormSubmissions((prevSubmissions) => [...prevSubmissions, data]);
   };
+  const onDelete = (index: number) => {
+    setFormSubmissions((prevSubmissions) => [
+      ...prevSubmissions.slice(0, index),
+      ...prevSubmissions.slice(index + 1),
+    ]);
+  };
+
+  const categoriesD = useMemo(() => {
+    const allCategories = data.map((item) => item.category);
+    return Array.from(new Set(allCategories));
+  }, [data]);
+
   return (
     <>
       <ExpenseForm onSubmit={dataHandler} />
@@ -38,6 +50,7 @@ function App() {
           </tbody>
         </table>
       )} */}
+      <TableFilter value={data} onDelete={onDelete} categories={categoriesD} />
     </>
   );
 }
---
+++ src/components/tableFilter/tableFilter.tsx
@@ -0,0 +1,78 @@
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Formcontainer from "../formcontainer";
+import "../styleComp.css";
+import { useState } from "react";
+
+interface datas {
+  description: string;
+  amount: Number;
+  category: string;
+}
+
+interface tabledata {
+  value: datas[];
+  onDelete: (index: number) => void;
+  categories: string[];
+}
+const tableFilter = ({ value, onDelete, categories }: tabledata) => {
+    const [selectedCategory, setSelectedCategory] = useState<string | "all">(
+      "all"
+    );
+
+    const filteredData =
+      selectedCategory === "all"
+        ? value
+        : value.filter((item) => item.category === selectedCategory);
+  return (
+    <>
+      {value.length > 0 && (
+        <Formcontainer>
+          <Form.Group className="mb-3">
+            <Form.Label>Disabled select menu</Form.Label>
+            <Form.Select
+              value={selectedCategory}
+              onChange={(e) => setSelectedCategory(e.target.value)}
+            >
+              <option value="all">All</option>
+              {categories.map((key,ind) => (
+                <option key={ind} value={key}>{key}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+          <Table striped bordered hover className="mt-3">
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.description}</td>
+                  <td>{item.amount.toString()}</td>
+                  <td>{item.category}</td>
+                  <td>
+                    <Button
+                      variant="light"
+                      className=" text-danger btnDelete"
+                      onClick={() => onDelete(index)}
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Formcontainer>
+      )}
+    </>
+  );
+};
+
+export default tableFilter;

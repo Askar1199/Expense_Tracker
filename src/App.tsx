3447e967045b20252ebbde158c3fc167aaf849aa
Mohammed Askar <mdaskar40@gmail.com>
2023-10-27 17:04:02 +0530

    add background color

--- src/App.tsx
+++ src/App.tsx
@@ -30,26 +30,7 @@ function App() {
   return (
     <>
       <ExpenseForm onSubmit={dataHandler} />
-      {/* {data.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th>Description</th>
-              <th>Amount</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => (
-              <tr key={index}>
-                <td>{item.description}</td>
-                <td>{item.amount.toString()}</td>
-                <td>{item.category}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )} */}
+      
       <TableFilter value={data} onDelete={onDelete} categories={categoriesD} />
     </>
   );
